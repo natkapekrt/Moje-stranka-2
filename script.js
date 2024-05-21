348555d5b1aaf27c19f0d865709e0a295dd68587
natkapekrt <147540885+natkapekrt@users.noreply.github.com>
2024-05-21 13:01:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Page loaded and ready.");
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Page loaded and ready!");
+});
